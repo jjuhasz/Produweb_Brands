fe5d48d03c567462a5ea4ef677cb7f05dc8ecbcd
Jonathan Juhasz <jonathan.juhasz@produweb.be>
2012-11-21 15:29:49 +0100

    PROTOZOOM dev

--- app/code/local/Produweb/ProtoZoom/Helper/Data.php
+++ app/code/local/Produweb/ProtoZoom/Helper/Data.php
@@ -6,6 +6,10 @@
  * Time: 0:57
  * To change this template use File | Settings | File Templates.
  */ 
-class Produweb_ProtoZoom_Helper_Data extends Mage_Core_Helper_Abstract {
-
+class Produweb_ProtoZoom_Helper_Data extends Mage_Core_Helper_Abstract
+{
+	public function getConfig($field, $group = "config", $section = "protozoom") {
+		$storeId = Mage::app()->getStore()->getId();
+		return Mage::getStoreConfig($section.'/'.$group.'/'.$field, $storeId);
+	}
 }
